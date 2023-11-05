995b7127ecdf5926895ff82be9f1059b4b3344ea
m2s <m.sohani@mci.dev.ocm>
2023-11-05 10:45:51 +0330

    add authorization config

--- src/main/java/com/truongbn/security/controller/AuthorizationController.java
+++ src/main/java/com/truongbn/security/controller/AuthorizationController.java
@@ -1,6 +1,7 @@
 package com.truongbn.security.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,5 +15,17 @@ public class AuthorizationController {
     @GetMapping
     public ResponseEntity<String> sayHello() {
         return ResponseEntity.ok("Here is your resource");
+    }
+
+    @GetMapping("/welcomeAdmin")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public String welcome() {
+        return "Welcome Admin";
+    }
+
+    @GetMapping("/welcomeUser")
+    @PreAuthorize("hasAuthority('USER')")
+    public String welcomeUser() {
+        return "Welcome USER";
     }
 }
